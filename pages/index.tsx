e63bc6cf4e961baa7b635b85a312d0ec11f936a6
vxxvvxxv <versus.sokol@gmail.com>
2021-12-21 00:52:42 +0600

    Write a blog in TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,58 @@
+import {GetStaticProps} from 'next'
+import Head from 'next/head';
+import Link from 'next/link'
+import Layout, {siteTitle} from '../components/layout';
+import Date from '../components/date'
+import utilStyles from '../styles/utils.module.css';
+import {getSortedPostsData} from '../lib/posts';
+
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm Vladimir Sokolovskiy. I'm software engineer.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br/>
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
